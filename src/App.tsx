e5ec5a42630c0403f2e352239f19085e56a91e65
zhi <1106010597@qq.com>
2021-08-14 13:27:43 +0800

    添加 svg 文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import {HashRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
+import styled from 'styled-components';
+import Nav from './components/Nav';
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display:flex;
+  flex-direction: column;
+`;
 
 function App() {
   return (
-   <div>hi</div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav/>
+      </Wrapper>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return (
+    <div>页面不存在，你丫输错地址了吧！</div>
+  );
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
 }
 
 export default App;
