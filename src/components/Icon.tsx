9c50653b476de032d43809f58818a09c8b0037a2
zhi <1106010597@qq.com>
2021-08-14 15:23:32 +0800

    创建 Icon 组件和 Layout 组件

---
+++ src/components/Icon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+try {
+  importAll(require.context('icons', true, /\.svg$/))
+}catch (e) {
+  console.log(e);
+}
+
+type Props = {
+  name: string
+}
+const Icon = (props:Props)=>{
+  return(
+    <svg className="icon">
+      <use xlinkHref={'#'+ props.name}/>
+    </svg>
+  )
+}
+export default Icon
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+import Icon from './Icon';
 
-require('icons/money.svg');
-require('icons/tag.svg');
-require('icons/chart.svg');
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -31,21 +29,15 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#tag"/>
-          </svg>
+          <Icon name="tag"/>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#money"/>
-          </svg>
+          <Icon name="money"/>
           <Link to="/money">记账页</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#chart"/>
-          </svg>
+          <Icon name="chart"/>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
