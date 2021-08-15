c646834aa1f2a922e56eff384a6cf1a94f0f0e0d
zhi <1106010597@qq.com>
2021-08-15 15:16:28 +0800

    money.tsx 之  UI 完成

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,7 +16,7 @@ const Wrapper = styled.div`
 const Layout = (props:any)=>{
   return(
     <Wrapper>
-      <Main>
+      <Main className={props.className}>
         {props.children}
       </Main>
       <Nav/>
