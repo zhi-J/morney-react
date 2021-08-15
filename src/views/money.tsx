6a2b5380619ccb21c6f3662d9e96bd3b9726d3af
zhi <1106010597@qq.com>
2021-08-15 16:18:27 +0800

    完成备注的功能

--- src/views/money.tsx
+++ src/views/money.tsx
@@ -2,7 +2,7 @@ import Layout from '../components/Layout';
 import React from 'react';
 import styled from 'styled-components';
 import {CategorySection} from './money/CategorySection';
-import {NotesSection} from './money/NotesSection';
+import {NoteSection} from './money/NoteSection';
 import {NumberPadSection} from './money/NumberPadSection';
 import {TagsSection} from './money/TagsSection';
 
@@ -13,15 +13,8 @@ const MyLayout = styled(Layout)`
 function Money() {
   return (
     <MyLayout>
-      <TagsSection>
-
-      </TagsSection>
-      <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder="在这里添加备注"/>
-        </label>
-      </NotesSection>
+      <TagsSection/>
+      <NoteSection/>
       <CategorySection>
         <ul>
           <li className="selected">支出</li>
