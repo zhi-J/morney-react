181c524074b9ab4382b79979750c7ec8442b1ff3
zhi <1106010597@qq.com>
2021-08-15 17:22:20 +0800

    完成 Category 功能

--- src/views/money/CategorySection.tsx
+++ src/views/money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   font-size: 24px;
   >ul{
     display: flex;
@@ -23,4 +24,23 @@ const CategorySection = styled.section`
     }
   }
 `;
-export {CategorySection}
+const CategorySection: React.FunctionComponent = () => {
+  const [category, setCategory] = useState('-');
+  return (
+    <Wrapper>
+      <ul>
+        <li className={category === '-' ? 'selected' : ''}
+            onClick={() => {
+              setCategory('-');
+            }}>支出
+        </li>
+        <li className={category === '+' ? 'selected' : ''}
+            onClick={() => {
+              setCategory('+');
+            }}>收入
+        </li>
+      </ul>
+    </Wrapper>
+  );
+};
+export {CategorySection};
