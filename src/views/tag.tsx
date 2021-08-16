07ce9f217cf7b9bf69d4b7a48de7adca554b9fca
zhi <1106010597@qq.com>
2021-08-16 15:03:52 +0800

    完成标签展示页面样式

--- src/views/tag.tsx
+++ src/views/tag.tsx
@@ -1,13 +1,54 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import {useTags} from '../useTags';
+import Icon from '../components/Icon';
+import styled from 'styled-components';
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+  >li{
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #F0C48A;
+  color: white;
+  border-radius: 4px;
+`;
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 48px 0;
+`;
 
 function Tags() {
-  const {tags, setTags} = useTags()
+  const {tags, setTags} = useTags();
   return (
     <Layout>
-      <h2>Tags</h2>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <span>{tag}</span>
+            <Icon name="right"/>
+          </li>
+        )}
+      </TagList>
+      <Center>
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 }
+
 export default Tags;
